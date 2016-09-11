62f50857b0af668feb7e63afba63da9090a240e5
lokeshwrn <lokeshwrn27@gmail.com>
2016-09-11 17:27:43 +0530

    changes for banner height

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -7,18 +7,21 @@ class BlogController < ApplicationController
   end
 
   def show_blog
+    @page_properties[:half_banner]=true
     @article=Article.published_blog.find_by_alias_url(params[:article_alias_name])
     render_404 and return if @article.blank?
     @article_content=@article.article_content
   end
 
   def tag_articles
+    @page_properties[:half_banner]=true
     @tag=Tag.find_by_alias_name(params[:tag_alias_name])
     render_404 if @tag.blank?
     @articles=Article.published_blog.by_tag(@tag.id)
   end
 
   def category_articles
+    @page_properties[:half_banner]=true
     @category=Category.find_by_alias_name(params[:category_alias_name])
     render_404 if @category.blank?
     @articles=Article.published_blog.by_category(@category.id)
